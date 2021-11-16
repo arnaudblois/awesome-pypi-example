bd76c014eabd682378c45a5545ad987ee1e64efb
Arnaud Blois <a.blois@ucl.ac.uk>
2021-11-16 23:55:52 +0000

    fix: getting the docs working
    also fixing cache issue

--- src/awesome_pypi_example/__init__.py
+++ src/awesome_pypi_example/__init__.py
@@ -1,3 +1,9 @@
-"""Adding the version of the package here to make it easily available."""
+"""Awesome Pypi Example.
 
+An example of working Python package built with Poetry, complete
+with tests and documentation, leveraging Github actions.
+"""
+
+# We define the version here to be able to access it
+# programmatically.
 __version__ = "0.1.0"
